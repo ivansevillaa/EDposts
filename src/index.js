be7ad2333a346ddd27140866b0872bf4556b2879
Ivan Sevilla <ivansevillaa@gmail.com>
2019-07-25 12:34:49 -0300

    created sotre

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import { App } from './App'
+
+const store = createStore(
+  {},
+  {}
+)
 
 render(<App />, document.getElementById('root'))
